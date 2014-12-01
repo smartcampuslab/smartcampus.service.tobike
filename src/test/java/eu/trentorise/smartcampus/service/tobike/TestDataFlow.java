51bdbae9688e3d02decefc4144d89d64ceb03ac6
raman <raman@fbk.eu>
2014-12-01 18:26:34 +0100

    test with subscriber

--- src/test/java/eu/trentorise/smartcampus/service/tobike/TestDataFlow.java
+++ src/test/java/eu/trentorise/smartcampus/service/tobike/TestDataFlow.java
@@ -2,6 +2,7 @@ package eu.trentorise.smartcampus.service.tobike;
 
 import it.sayservice.platform.client.InvocationException;
 import it.sayservice.platform.client.ServiceBusClient;
+import it.sayservice.platform.client.ServiceBusListener;
 import it.sayservice.platform.client.jms.JMSServiceBusClient;
 import it.sayservice.platform.core.message.Core.ActionInvokeParameters;
 import it.sayservice.platform.servicebus.test.DataFlowTestHelper;
@@ -16,6 +17,7 @@ import junit.framework.TestCase;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Message;
 
 import eu.trentorise.smartcampus.service.tobike.data.message.Tobike.Stazione;
@@ -47,6 +49,18 @@ public class TestDataFlow extends TestCase {
 
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
 		ServiceBusClient client = new JMSServiceBusClient(connectionFactory);
+		client.setClientId("test_subscriber");
+		
+		client.setListener(new ServiceBusListener() {
+			
+			@Override
+			public void onServiceEvents(String arg0, String arg1, String arg2, List<ByteString> arg3) {
+				System.err.println("RECEIVED "+ arg3.size());
+			}
+		});
+		
+		client.subscribeService("smartcampus.service.tobike", "GetStazioni", parameters);
+		
 		ActionInvokeParameters invokeService = client.invokeService("smartcampus.service.tobike", "GetStazioni", parameters);
 		System.err.println(invokeService.getDataCount());
 	}
