36f23acf953deee2b5596e0fcfa548220a438e71
gioadami <giordano.adami@gmail.com>
2016-09-16 17:02:42 +0200

    Fixed charset conversion.

--- src/main/java/eu/trentorise/smartcampus/service/tobike/scripts/TobikeScript.java
+++ src/main/java/eu/trentorise/smartcampus/service/tobike/scripts/TobikeScript.java
@@ -15,9 +15,10 @@
  ******************************************************************************/
 package eu.trentorise.smartcampus.service.tobike.scripts;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.codec.Charsets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.Message;
@@ -36,7 +37,7 @@ public class TobikeScript {
 		TOBikeUtente wsc = new TOBikeUtente();
 		String s =  wsc.getTOBikeUtenteSoap().elencoStazioniPerComuneJSON(user, password, code);
 		s = s.substring(1, s.length() - 1);
-		s = new String(s.getBytes("ISO-8859-1"), Charset.forName("UTF-8"));
+		s = new String(s.getBytes("Windows-1252"), Charsets.UTF_8);
 		
 		String stazioni[] = s.split("\\|");
 		
